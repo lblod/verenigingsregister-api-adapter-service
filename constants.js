5650911ecb598bf2f3e3d5038892f0339717fb76
Boris De Vloed <code@devloed.com>
2025-06-02 09:31:47 +0200

    wip

--- constants.js
+++ constants.js
@@ -6,3 +6,4 @@ export const AUD = process.env.AUD;
 export const AUTH_DOMAIN = process.env.AUTH_DOMAIN || 'authenticatie.vlaanderen.be';
 export const API_URL = process.env.API_URL || 'https://iv.api.vlaanderen.be/api/v1/organisaties/verenigingen/';
 export const API_VERSION = process.env.API_VERSION || 'v1';
+export const EDITOR_ROLE = 'verenigingen-beheerder';
